e4a366308571cbf2c8666ad09bfd964b669226a2
Nevylish <nevylish@nevylish.fr>
2022-08-31 02:58:56 +0200

    add: Ligne commentée

--- client.lua
+++ client.lua
@@ -11,6 +11,7 @@ end
 
 RegisterCommand('carkill', function()
     if checkCauseOfDeath(carkill, GetPedCauseOfDeath(GetPlayerPed(-1))) then
+        -- If your not using ESX framework, you can replace triggers
         TriggerEvent('esx_ambulancejob:revive')
         TriggerEvent('esx:showNotification', '~g~Vous avez été réanimé suite à une mort causé par un véhicule.')
     else
